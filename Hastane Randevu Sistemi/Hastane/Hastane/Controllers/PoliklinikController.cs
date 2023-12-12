0ace2b60f3d5cd63e2821cf95f43050db9527a9b
Feyza Nur Özdemir <83233400+feyzaozdmir@users.noreply.github.com>
2023-12-13 02:09:31 +0300

    Update

---
+++ Hastane Randevu Sistemi/Hastane/Hastane/Controllers/PoliklinikController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PoliklinikController : Controller
+{
+    private List<string> poliklinikListesi = new List<string>
+    {
+        "Dahiliye", "Göz Hastalıkları", "Ortopedi", "Kardiyoloji",
+        "Cildiye", "Nöroloji", "Üroloji", "Psikiyatri","Acil Servis",
+        "Beyin ve Sinir Cerrahisi","Çocuk Sağlığı ve Hastalıkları","Dermatoloji",
+        "Genel Cerrahi"
+        // poliklinik ekle buraya
+    };
+
+    public IActionResult Index()
+    {
+        
+        var grupluPoliklinikler = poliklinikListesi
+            .OrderBy(poliklinik => poliklinik)
+            .GroupBy(poliklinik => poliklinik[0].ToString())
+            .ToDictionary(group => group.Key, group => group.ToList());
+
+        return View(grupluPoliklinikler);
+    }
+}
