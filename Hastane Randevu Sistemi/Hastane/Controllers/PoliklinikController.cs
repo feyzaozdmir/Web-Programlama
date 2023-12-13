e57f585a4e1ec889fd6466afc6747c5a2e14a273
Feyza Nur Özdemir <83233400+feyzaozdmir@users.noreply.github.com>
2023-12-13 22:57:01 +0300

    Güncelleme
    Ana Sayfa, Doktor, Poliklinik ve İletişim sayfaları oluşturuldu.

--- Hastane Randevu Sistemi/Hastane/Controllers/PoliklinikController.cs
+++ Hastane Randevu Sistemi/Hastane/Controllers/PoliklinikController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 public class PoliklinikController : Controller
 {
@@ -9,8 +10,14 @@ public class PoliklinikController : Controller
         "Dahiliye", "Göz Hastalıkları", "Ortopedi", "Kardiyoloji",
         "Cildiye", "Nöroloji", "Üroloji", "Psikiyatri","Acil Servis",
         "Beyin ve Sinir Cerrahisi","Çocuk Sağlığı ve Hastalıkları","Dermatoloji",
-        "Genel Cerrahi"
-        // poliklinik ekle buraya
+        "Genel Cerrahi","Aferez","Ağız ve Diş Sağlığı","Alerji ve İmmünoloji","Algoloji (Ağrı Polikliniği)",
+        "Anestezi ve Reanimasyon","Beslenme ve Diyet","Biyokimya Laboratuvarı","Böbrek Nakli","Check-up",
+        "Çocuk Alerjisi","Çocuk Cerrahisi","Çocuk Endokrinolojisi","Çocuk Gastroenteroloji","Çocuk Gelişim Danışmanı",
+        "Çocuk Hematolojisi ve Onkolojisi","Çocuk Kardiyolojisi","Çocuk Kardiyovasküler Cerrahisi","Çocuk Nefrolojisi",
+        "Çocuk Nörolojisi","Çocuk Psikoloji","Çocuk Romatolojisi","Çocuk ve Ergen Psikiyatrisi","Dil ve Konuşma Terapisi",
+        "Diyaliz","El Cerrahisi ve Mikro Cerrahi","Endokrinoloji ve Metabolizma Hastalıkları","Enfeksiyon Hastalıkları",
+        "Fizik Tedavi ve Rehabilitasyon","Havacılık Tıp Merkezi","Hematoloji","Hemodiyaliz","İç Hastalıkları","Jinekolojik Onkoloji",
+        "Kadın Hastalıkları ve Doğum","Laboratuvar","Mikrobiyoloji","Radyasyon Onkolojisi","Radyoloji","Tüp Bebek","Uyku Laboratuvarı"
     };
 
     public IActionResult Index()
--- Hastane Randevu Sistemi/Hastane/obj/Debug/net6.0/Hastane.AssemblyInfo.cs
+++ Hastane Randevu Sistemi/Hastane/obj/Debug/net6.0/Hastane.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hastane")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a2998d8df561fc88803e5b8205ee3dd4d5b77d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hastane")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hastane")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
